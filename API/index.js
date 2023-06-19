541997c8f47a30ab569deb280f9837096d3123bf
Andres Publio Gentile <a.gentile@correo.unimet.edu.ve>
2023-06-18 23:44:52 -0400

    add comments to the code

--- API/index.js
+++ API/index.js
@@ -8,8 +8,10 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors({ origin: true }));
 
-app.use(bodyParser.json()); // Analiza el cuerpo de la solicitud como JSON
-app.use(bodyParser.urlencoded({ extended: false })); // Analiza el cuerpo de la solicitud como URL codificado
+// Analiza el cuerpo de la solicitud como JSON
+app.use(bodyParser.json()); 
+// Analiza el cuerpo de la solicitud como URL codificado
+app.use(bodyParser.urlencoded({ extended: false })); 
 
 
 var serviceAccount = require("./permissions.json");
@@ -21,9 +23,8 @@ admin.initializeApp({
 const db = admin.firestore();
 
 
-// read all items from a collection
-// params => collection_name
 
+// Agrega una ruta GET a la aplicación Express que acepta un parámetro de nombre de colección. Cuando se llama a esta ruta, el código consulta la base de datos Firestore para obtener todos los documentos de la colección especificada y los devuelve en un objeto de respuesta JSON. Si se produce un error durante la consulta a la base de datos, el código devuelve un error de estado 500 junto con el mensaje de error.
 app.get('/read/:collection_name', (req, res) => {
     (async () => {
         try {
@@ -49,7 +50,7 @@ app.get('/read/:collection_name', (req, res) => {
 
 const chatCollectionRef = db.collection('chat');
 
-
+// Agrega una ruta POST a la aplicación Express que acepta los datos de entrada en formato JSON. Cuando se llama a esta ruta, el código extrae los datos de entrada de la solicitud HTTP POST y crea un objeto chatItem con el contenido de la solicitud y la respuesta de chat. Luego, el código agrega el objeto chatItem como un nuevo documento en la colección de chat en la base de datos Firestore y devuelve un objeto JSON con el ID del nuevo documento creado. Si se produce un error al agregar el nuevo documento a la base de datos, el código devuelve un error de estado 500 junto con un mensaje de error.
 app.post('/chat', (req, res) => {
     console.log('Body de la solicitud HTTP POST:', req.body); // Imprime el cuerpo de la solicitud HTTP POST
   const chatItem = {
